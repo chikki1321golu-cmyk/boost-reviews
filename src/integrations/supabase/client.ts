576fa2bc95d851caec58958e64c0fbe5b67adc61
chikki1321golu-cmyk <chikki1321golu@gmail.com>
2026-04-04 11:40:32 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = "https://yqqkhvrgiclvfxobnnhw.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxcWtodnJnaWNsdmZ4b2Jubmh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NDMzNDIsImV4cCI6MjA5MDUxOTM0Mn0.OzeNhQ5pmiJfPFGzJ1upu4OmRA20uF0sgonRrTXdGHo";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
